af82cc20d25de018f3640c1acbfe7906d3e557f7
Nicolas Araujo <araujo.n@northeastern.edu>
2023-08-10 21:02:31 -0700

    copy dumb evil bot and convert evil bot to have my bot features without transposition table

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,5 +1,8 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChessChallenge.Example
 {
@@ -7,48 +10,150 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        private bool IsWhite;
+        private readonly int[] PieceValues = new int[7] { 0, 1, 3, 3, 5, 9, 0 };
 
-        public Move Think(Board board, Timer timer)
+        private int CalculateScore(Board board, bool white)
         {
-            Move[] allMoves = board.GetLegalMoves();
-
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
-
-            foreach (Move move in allMoves)
+            int score = 0;
+            foreach (PieceType pieceType in Enum.GetValues(typeof(PieceType)))
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
+                if (pieceType == PieceType.None || pieceType == PieceType.King)
                 {
-                    moveToPlay = move;
-                    break;
+                    continue;
                 }
-
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-                if (capturedPieceValue > highestValueCapture)
+                PieceList pieceList = board.GetPieceList(pieceType, white);
+                for (int i = 0; i < pieceList.Count; i++)
                 {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
+                    Piece piece = pieceList.GetPiece(i);
+                    int pieceValue = PieceValues[(int)piece.PieceType];
+                    score += pieceValue;
+                }
+            }
+            return score;
+        }
+        // public EvaluationData whiteEval;
+        // public EvaluationData blackEval;
+
+        // Performs static evaluation of the current position.
+        // The position is assumed to be 'quiet', i.e no captures are available that could drastically affect the evaluation.
+        // The score that's returned is given from the perspective of whoever's turn it is to move.
+        // So a positive score means the player who's turn it is to move has an advantage, while a negative score indicates a disadvantage.
+        public double Evaluate(Board board)
+        {
+            if (board.IsDraw()) { return 0; }
+            if (board.IsInCheckmate())
+            {
+                if (board.IsWhiteToMove == IsWhite)
+                {
+                    return double.NegativeInfinity;
+                }
+                else
+                {
+                    return double.PositiveInfinity;
+                }
+            }
+            int whiteMaterialScore = CalculateScore(board, true);
+            int blackMaterialScore = CalculateScore(board, false);
+
+            int perspective = IsWhite ? 1 : -1;
+
+            int eval = whiteMaterialScore - blackMaterialScore;
+            return eval * perspective;
+        }
+
+        public Move? BestMove(Board board, int depth)
+        {
+            List<Move> bestMoves = new();
+            double maxEvaluation = double.NegativeInfinity;
+
+
+            foreach (Move move in board.GetLegalMoves())
+            {
+                board.MakeMove(move);
+                double currentEvaluation = Minimax(board, depth - 1, double.NegativeInfinity, double.PositiveInfinity, false);
+                board.UndoMove(move);
+                if (currentEvaluation > maxEvaluation)
+                {
+                    maxEvaluation = currentEvaluation;
+                    bestMoves = new List<Move> { move };
+                }
+                else if (currentEvaluation == maxEvaluation)
+                {
+                    bestMoves.Add(move);
                 }
             }
 
-            return moveToPlay;
+            return bestMoves[new Random().Next(bestMoves.Count)];
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
+        private double Minimax(Board board, int depth, double alpha, double beta, bool isMaximizing)
         {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
+            if (depth == 0 || IsGameOver(board))
+            {
+                return Evaluate(board);
+            }
+
+            if (isMaximizing)
+            {
+                double maxEval = double.NegativeInfinity;
+                foreach (Move move in board.GetLegalMoves())
+                {
+                    board.MakeMove(move);
+                    double eval = Minimax(board, depth - 1, alpha, beta, false);
+                    board.UndoMove(move);
+                    maxEval = Math.Max(maxEval, eval);
+                    alpha = Math.Max(alpha, eval);
+                    if (beta <= alpha)
+                    {
+                        break;  // Alpha-beta pruning
+                    }
+                }
+                return maxEval;
+            }
+            else
+            {
+                double minEval = double.PositiveInfinity;
+                foreach (Move move in board.GetLegalMoves())
+                {
+                    board.MakeMove(move);
+                    double eval = Minimax(board, depth - 1, alpha, beta, true);
+                    board.UndoMove(move);
+                    minEval = Math.Min(minEval, eval);
+                    beta = Math.Min(beta, eval);
+                    if (beta <= alpha)
+                    {
+                        break;  // Alpha-beta pruning
+                    }
+                }
+                return minEval;
+            }
+        }
+
+
+        private bool IsGameOver(Board board)
+        {
+            return board.IsDraw() || board.IsInCheckmate();
+        }
+
+        public Move Think(Board board, Timer timer)
+        {
+            IsWhite = board.IsWhiteToMove;
+            int count = BitOperations.PopCount(board.AllPiecesBitboard);
+            int depth = 4;
+            if (count < 10)
+            {
+                depth = 6;
+            }
+            Move? bestMove = BestMove(board, depth);
+            if (bestMove.HasValue)
+            {
+                return bestMove.Value;
+            }
+            else
+            {
+                return board.GetLegalMoves().First();
+            }
         }
     }
 }
